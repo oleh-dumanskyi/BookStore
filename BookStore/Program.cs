be6c10efa92edf8dd0a345e19ed9d2d8048067b4
oleh-dumanskyi <dasgo121@gmail.com>
2022-05-30 22:13:23 +0300

    Configured template

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -5,9 +5,20 @@ namespace BookStore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
