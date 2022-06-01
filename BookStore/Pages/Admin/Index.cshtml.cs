e2adfa059c4febf7d16e17a358c4f52e245c9306
oleh-dumanskyi <dasgo121@gmail.com>
2022-06-01 23:33:31 +0300

    Updated page file structure

--- BookStore/Pages/Admin/Index.cshtml.cs
+++ BookStore/Pages/Admin/Index.cshtml.cs
@@ -1,10 +1,16 @@
+using BookStore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BookStore.Pages.Admin
 {
+    [Authorize(Roles = "Admin")]
     public class IndexModel : PageModel
     {
+        public ApplicationDbContext _context { get; set; }
+        public IndexModel(ApplicationDbContext context) => _context = context;
+
         public void OnGet()
         {
         }
---
+++ BookStore/Pages/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages.Admin.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
